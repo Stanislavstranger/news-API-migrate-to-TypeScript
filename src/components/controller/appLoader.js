da8d3a18f7d043e9f48976eb6f31137dc3ad6fb5
Stanislav Goloborodov <stanislavstranger@gmail.com>
2023-06-15 17:56:27 +0300

    feat: add typescript, eslint settings

--- src/components/controller/appLoader.js
+++ src/components/controller/appLoader.js
@@ -3,7 +3,7 @@ import Loader from './loader';
 class AppLoader extends Loader {
     constructor() {
         super('https://newsapi.org/v2/', {
-            apiKey: '', // получите свой ключ https://newsapi.org/
+            apiKey: '044e7209577f487e888e86ddd5e4ad00', // получите свой ключ https://newsapi.org/
         });
     }
 }
